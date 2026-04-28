465965d6087be6a08859e1785ed43b477129c9bb
shaadali-tech <215ics043@gbu.ac.in>
2026-04-28 12:32:53 +0530

    add hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setdata] = useState("");
   return (
     <>
       <input type="text" placeholder="Enter Here" />
+      <button
+        onClick={() => {
+          setdata("Updated Value");
+        }}
+      >
+        Update
+      </button>
+      <h1>{data}</h1>
     </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,20 @@ import App from "./App";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { test, expect } from "vitest";
 
+beforeEach(() => {
+  console.log("Before Each Test");
+});
+
 test("should 1st Test", () => {
   render(<App />);
   const input = screen.getByRole("textbox");
   fireEvent.change(input, { target: { value: "Hello World" } });
   expect(input.value).toBe("Hello World");
+});
+
+test("should 2nd Test", () => {
+  render(<App />);
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
+  expect(screen.getByText("Updated Value")).toBeInTheDocument();
 });
