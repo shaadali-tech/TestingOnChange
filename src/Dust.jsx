4c45d41fd22521c5b260edc5fcf207333f8df11e
shaadali-tech <215ics043@gbu.ac.in>
2026-04-30 11:37:21 +0530

    display value

--- src/Dust.jsx
+++ src/Dust.jsx
@@ -4,6 +4,7 @@ const Dust = () => {
       <h1>Dust</h1>
       <input type="text" />
       <button data-testid="btn-test-id"> Click Me</button>
+      <textarea name="texting" defaultValue="Shaad"></textarea>
     </div>
   );
 };
--- src/Dust.test.jsx
+++ src/Dust.test.jsx
@@ -1,16 +1,23 @@
 import Dust from "./Dust";
 import { render, screen } from "@testing-library/react";
 
-test("get by role", () => {
+/*test("get by role", () => {
   render(<Dust />);
 
   const input = screen.getByRole("textbox");
   expect(input).toBeInTheDocument();
-});
+});*/
 
 test("get by test-id", () => {
   render(<Dust />);
 
   const button = screen.getByTestId("btn-test-id");
   expect(button).toBeInTheDocument();
+});
+
+test("get by Default Value", () => {
+  render(<Dust />);
+
+  const textarea = screen.getByDisplayValue("Shaad");
+  expect(textarea).toBeInTheDocument();
 });
